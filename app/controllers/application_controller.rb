f99b2f229bac80f7bfd4d2d74fb80484f6b3f2a8
Viktharien <viktharien@zoho.com>
2018-05-03 20:31:53 +0000

    configure application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,15 @@
 class ApplicationController < ActionController::Base
     include Response
+    before_action :configure_permitted_parameters, if: :devise_controller?
+    
     def not_found
         redirect_to '/public/404.html'
+    end
+
+    
+    protected
+    
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:fullname])
     end
 end
