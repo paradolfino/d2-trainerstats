e9c145b6d97262fef07fc5f760c1b3059b6efc4e
Viktharien <viktharien@zoho.com>
2018-05-04 21:43:17 +0000

    implement admin functionality for adding new users

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -6,6 +6,11 @@ class RegistrationsController < ApplicationController
     end
     
     def create
-       @user = User.new(params) 
+       @user = User.new(params)
+       sql = "insert into users (name,email, created_at,updated_at) values( 
+        #{ActiveRecord::Base.connection.quote(user_params[:fullame])}, 
+        #{ActiveRecord::Base.connection.quote(user_params[:email])},now(), now())"
+  ActiveRecord::Base.connection.execute(sql)
+  redirect_to action: 'index'
     end
 end
