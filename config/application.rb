4ccd06b6075a5ccb688f2af05df61bf5562b691f
Viktharien <viktharien@zoho.com>
2018-05-04 21:37:16 +0000

    implement admin functionality for adding new users

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,6 @@ module TrainerStats
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-    paths.app.views << "app/views/devise"
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
