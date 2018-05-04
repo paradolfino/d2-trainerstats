8b7b60bdda92645356c463859d7773418055dde2
Viktharien <viktharien@zoho.com>
2018-05-04 22:05:59 +0000

    implement admin functionality for adding new users

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => {:registrations => "registrations"}
+  devise_for :users
 =begin 
   root 'trainings#new'
   post '/' => 'trainings#create'
