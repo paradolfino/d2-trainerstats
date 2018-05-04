e01f5256f6fa544413960746bb0ceb72d87af63d
Viktharien <viktharien@zoho.com>
2018-05-04 21:35:11 +0000

    implement admin functionality for adding new users

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,10 +1,10 @@
-class UsersController < ApplicationController
+class RegistrationsController < ApplicationController
     def new
         redirect_to '/' unless user_signed_in? && current_user.role == "admin" 
         @user = User.new
     end
     
     def create
-        
+       @user = User.new(params) 
     end
 end
