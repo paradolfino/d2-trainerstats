8154bbe8d9a598d50f7dfa490528b4fde7cbb9d9
Viktharien <viktharien@zoho.com>
2018-05-04 22:12:34 +0000

    implement admin functionality for adding new users

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   devise_scope :user do
     get 'login', to: 'devise/sessions#new'
     get 'logout', to: 'devise/sessions#destroy'
-    get 'sign_up', to: 'registrations#new'
+    get 'sign_up', to: 'users##new'
     
   end
   resources :users
