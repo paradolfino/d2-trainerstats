c5f99992f7e3fc2c3b295a87989338f1b4cd2145
Viktharien <viktharien@zoho.com>
2018-05-07 16:49:31 +0000

    implement admin menu (users index)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-    before_action :set_user, only [:edit, :update, :destroy]
+    before_action :set_user, only: [:edit, :update, :destroy]
     before_action :authenticate_user!
     before_action :require_admin, only: [:new, :index, :destroy]
     
