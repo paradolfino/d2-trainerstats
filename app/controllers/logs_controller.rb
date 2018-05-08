2de99512bbb05244f7d333fbdbd23eabef7bd0b0
Viktharien <viktharien@zoho.com>
2018-05-08 19:54:12 +0000

    implement event creation on creation of logs

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -62,7 +62,7 @@ class LogsController < ApplicationController
         @log = Log.find(params[:id])
         @old_title = @log.title
         if @log.update(params_log)
-            create_event("updated", "log(Title: #{@old_title}) to (Title: #{@log.title}, Active: #{@log.active})")
+            create_event("updated", "log(ID: #{@log.id}, Title: #{@old_title}) to (Title: #{@log.title}, Active: #{@log.active})")
             redirect_to logs_path
         else
             render 'edit'
@@ -70,7 +70,7 @@ class LogsController < ApplicationController
     end
     
     def destroy
-        create_event("destroyed", "log(Title: #{@log.title})")
+        create_event("destroyed", "log(ID: #{@log.id}, Title: #{@log.title})")
         @log.destroy
         redirect_to logs_path
         
