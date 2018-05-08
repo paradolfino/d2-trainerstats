20f531a50b8e86bd6a66a1ca34a12c39195f8ecd
Viktharien <viktharien@zoho.com>
2018-05-08 19:54:14 +0000

    implement event creation on creation of logs

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -48,7 +48,7 @@ class LogsController < ApplicationController
     def create
         @log = Log.new(params_log)
         if @log.save
-            create_event("created", "log(Title: #{@log.title}, Active: #{@log.active})")
+            create_event("created", "log(ID: #{@log.id}, Title: #{@log.title}, Active: #{@log.active})")
             redirect_to @log
         else
             render 'new'
