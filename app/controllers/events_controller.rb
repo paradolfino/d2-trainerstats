f7f15467ea6f0683d3f4b0b8a115913b3457467b
Viktharien <viktharien@zoho.com>
2018-05-08 18:54:52 +0000

    create events controller and actions

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,10 @@
 class EventsController < ApplicationController
+    
+    
+    private
+        
+        def event_params
+           params.require(:event).permit(:action, :name, :user_id) 
+        end
+    
 end
