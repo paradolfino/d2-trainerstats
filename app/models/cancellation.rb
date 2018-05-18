09e0193fe4c6953c8ffe1d3c27e4d941e707a22f
Viktharien <viktharien@zoho.com>
2018-05-18 20:26:50 +0000

    set up cancel routes and views

--- app/models/cancellation.rb
+++ app/models/cancellation.rb
@@ -1,3 +1,14 @@
 class Cancellation < ApplicationRecord
   belongs_to :cancellation_log
+  def self.to_csv(options = {})
+        items = 0
+      CSV.generate(options) do |csv|
+        csv << column_names
+        all.each do |cancellation|
+            items += 1
+          csv << cancellation.attributes.values_at(*column_names)
+        end
+        csv << ["Total Cancellations", items]
+      end
+    end
 end
