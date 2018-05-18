60cd75f1e095744c30f82b06305874579d9e2276
Viktharien <viktharien@zoho.com>
2018-05-18 19:59:12 +0000

    set up cancel routes and views

--- config/routes.rb
+++ config/routes.rb
@@ -24,6 +24,9 @@ Rails.application.routes.draw do
   resources :logs do
     resources :trainings
   end
+  resources :cancellation_logs do
+    resources :cancellations
+  end
   
   get '/public/404.html' => 'logs#index'
   
