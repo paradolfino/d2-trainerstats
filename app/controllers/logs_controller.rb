792f3347a9bfe050f5a2bafe00166610bddd9d62
Viktharien <viktharien@zoho.com>
2018-06-01 17:31:07 +0000

    set up logs controller for search action

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -5,6 +5,8 @@ class LogsController < ApplicationController
     before_action :authenticate_user!, except: [:index, :show]
     
     def search
+        @logs = Log.all
+        
         if params[:query]
             case params[:type]
             
