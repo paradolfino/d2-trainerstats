dd85c8e3d5a66548cc4283deccc67cb39a2eaa80
Viktharien <viktharien@zoho.com>
2018-06-05 21:50:01 +0000

    create helper method to parse search params

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,17 +1,17 @@
 module ApplicationHelper
     def sanitize_search(query)
         case query
-            when "Member Name"
-                "member"
-            when "Company Name"
-                "company"
-            when "Stage"
-                query.downcase
-            when "Status"
-                query.downcase
-            else
-                "member"
-            end
+        when "Member Name"
+            "member"
+        when "Company Name"
+            "company"
+        when "Stage"
+            query.downcase
+        when "Status"
+            query.downcase
+        else
+            "member"
+        end
     end
     def search_compare(query, string)
         return true if query.include? string.downcase
