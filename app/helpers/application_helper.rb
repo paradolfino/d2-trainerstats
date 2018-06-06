d0341ac6f55f576105eab1a2ea6f8c75e4478d07
Viktharien <viktharien@zoho.com>
2018-06-06 21:59:10 +0000

    fix issue where search_compare helper was not parsing params correctly

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,19 +1,5 @@
 module ApplicationHelper
-    def sanitize_search(query)
-        case query
-        when "Member Name"
-            "member"
-        when "Company Name"
-            "company"
-        when "Stage"
-            query.downcase
-        when "Status"
-            query.downcase
-        else
-            "member"
-        end
-    end
-    def search_compare(query, string)
-        return true if query.include? string.downcase
+    def search_compare(value, string)
+        return true if value.downcase.include? string.downcase
     end
 end
