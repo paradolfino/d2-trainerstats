95090b2789fbfa486aa20d49bbf7c7149081bd2d
Viktharien <viktharien@zoho.com>
2018-06-13 20:28:11 +0000

    fix trainings destroy action to redirect to :back instead of @log

--- app/controllers/trainings_controller.rb
+++ app/controllers/trainings_controller.rb
@@ -47,7 +47,7 @@ class TrainingsController < ApplicationController
         @training = @log.trainings.find(params[:log_id])
         create_event("destroyed", "#{@training.status} training for #{@training.member} from #{@training.company}")
         @training.destroy 
-        redirect_to current_page
+        redirect_to current_page_url
     end
     
     private
