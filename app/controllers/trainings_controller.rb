bb6a3892cf26c381d8c72a59e967671b30a721b5
Viktharien <viktharien@zoho.com>
2018-06-13 20:39:37 +0000

    fix trainings destroy action to redirect to :back instead of @log

--- app/controllers/trainings_controller.rb
+++ app/controllers/trainings_controller.rb
@@ -47,7 +47,7 @@ class TrainingsController < ApplicationController
         @training = @log.trainings.find(params[:log_id])
         create_event("destroyed", "#{@training.status} training for #{@training.member} from #{@training.company}")
         @training.destroy 
-        redirect_to :back
+        redirect_back fallback_location: root_path
     end
     
     private
